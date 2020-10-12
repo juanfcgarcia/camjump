ec37ee7a2a4fc5b0d695d3ed29e8435e356190cf
Danilo <nicolasdpf@gmail.com>
2020-10-12 15:09:27 -0500

    Initial commit

--- ball.js
+++ ball.js
@@ -1,66 +1,61 @@
-class Ball{
-x= width/2;
-y=50;
+class Ball {
+  x = width / 2;
+  y = 50;
 
-gravedad= 0.1;
-velocidad= 0;
-caida=false;
+  gravedad = 0.1;
+  velocidad = 0;
+  caida = false;
 
-    show = function(){
-        fill(255);
-        ellipse(this.x, this.y, 18, 18);
+  show = function () {
+    fill(255);
+    ellipse(this.x, this.y, 18, 18);
+  };
+
+  MoveLeft = function () {
+    if (this.x > 10) {
+      this.x -= 6;
+    }
+  };
+
+  MoveRight = function () {
+    if (this.x < width - 6) {
+      this.x += 6;
+    }
+  };
+
+  Jump = function () {
+    this.y -= 6;
+  };
+
+  DROP = function () {
+    this.gravedad = 0.0;
+    this.velocidad += this.gravedad;
+    this.y += this.velocidad;
+  };
+
+  UP = function () {
+    this.velocidad = -1.0;
+    this.y += this.velocidad;
+  };
+
+  update = function () {
+    if (this.caida == true) {
+      this.gravedad = 0.1;
+      this.velocidad += this.gravedad;
+      this.y += this.velocidad;
     }
 
-    MoveLeft = function(){
-        if(this.x>10){
-        this.x -=6;
-        }
+    if (this.y > height) {
+      //El limite de abajo
+      this.y = height;
+      this.velocidad = 0;
+      console.log(this.y);
     }
 
-    MoveRight = function(){
-        if(this.x<(width-6)){
-        this.x +=6;
-        }
-
+    if (this.y <= 0) {
+      //El limite de arriba
+      this.y = 0;
+      this.velocidad = 0;
     }
-
-    Jump = function(){
-        this.y -=6;
-    }
-
-    DROP = function(){
-        this.gravedad= 0.00;
-        this.velocidad +=this.gravedad;
-        this.y += this.velocidad;
-       
-    }
-
-    UP = function(){
-        this.velocidad =-1.0;
-        this.y += this.velocidad;
-     
-    }
-
-
-    update = function(){
-        if(this.caida==true){
-            this.gravedad= 0.1;
-            this.velocidad +=this.gravedad;
-            this.y += this.velocidad;
-        
-        }
-
-        if (this.y > height){ //El limite de abajo
-            this.y = height;
-            this.velocidad=0;
-            console.log(this.y);
-        }
-
-        if (this.y <= 0){ //El limite de arriba
-            this.y = 0;
-            this.velocidad=0;
-        }
-
-    }
-
+  };
 }
--- sketch.js
+++ sketch.js
@@ -4,136 +4,116 @@ let plataformas = [];
 let colores;
 let trackingData;
 
-let puede=0;
+let puede = 0;
 
-let separacion_y=580;
+let separacion_y = 580;
 
-function setup(){
-    createCanvas(400,600);
-    video = createCapture(VIDEO);
-    video.size(400, 600);
+function setup() {
+  createCanvas(400, 600);
+  video = createCapture(VIDEO);
+  video.size(400, 600);
 
-    video.style('opacity',1)
-    video.id("entradaVideo"); 
-    tracking.ColorTracker.registerColor('blue', function(r, g, b) {
+  video.style("opacity", 1);
+  video.id("entradaVideo");
+  tracking.ColorTracker.registerColor("blue", function (r, g, b) {
     var thresholdGreen = 50,
       thresholdBlue = 70,
       dx = r - 0,
       dy = g - 255,
       dz = b - 255;
 
-      if ((g - r) >= thresholdGreen && (b - r) >= thresholdBlue) {
-        
-        return true;
-      }
-      return dx * dx + dy * dy + dz * dz < 6400;
-    });
-    tracking.ColorTracker.registerColor('red', function(r, g, b) {
-      var threshold = 50,
-        dx = r - 255,
-        dy = g - 0,
-        dz = b - 255;
-  
-      if ((r - g) >= threshold && (b - g) >= threshold) {
-       
-        return true;
-      }
-      return dx * dx + dy * dy + dz * dz < 19600;
-    });
-  tracking.ColorTracker.registerColor('green', function(r, g, b) {
-      var threshold = 50,
-        dx = r - 255,
-        dy = g - 255,
-        dz = b - 0;
-  
-      if ((r - b) >= threshold && (g - b) >= threshold) {
-       
-        return true;
-      }
-      return dx * dx + dy * dy + dz * dz < 10000;
+    if (g - r >= thresholdGreen && b - r >= thresholdBlue) {
+      return true;
+    }
+    return dx * dx + dy * dy + dz * dz < 6400;
   });
-  colores = new tracking.ColorTracker(['red', 'green', 'blue']);
-   
-  
-    tracking.track('#entradaVideo', colores);
-  
-  
-    colores.on('track', function(event) { 
-        trackingData = event.data 
-    });
+  tracking.ColorTracker.registerColor("red", function (r, g, b) {
+    var threshold = 50,
+      dx = r - 255,
+      dy = g - 0,
+      dz = b - 255;
 
+    if (r - g >= threshold && b - g >= threshold) {
+      return true;
+    }
+    return dx * dx + dy * dy + dz * dz < 19600;
+  });
+  tracking.ColorTracker.registerColor("green", function (r, g, b) {
+    var threshold = 50,
+      dx = r - 255,
+      dy = g - 255,
+      dz = b - 0;
 
-    ball =  new Ball();
-    plataformas.push(new Plataforma());
-    
+    if (r - b >= threshold && g - b >= threshold) {
+      return true;
+    }
+    return dx * dx + dy * dy + dz * dz < 10000;
+  });
+  colores = new tracking.ColorTracker(["red", "green", "blue"]);
+
+  tracking.track("#entradaVideo", colores);
+
+  colores.on("track", function (event) {
+    trackingData = event.data;
+  });
+
+  ball = new Ball();
+  plataformas.push(new Plataforma());
 }
 
-function draw(){
-    keyPresionado();
-    background(12, 36, 97);
-    image(video, 0,0, width, height);
+function draw() {
+  keyPresionado();
+  background(12, 36, 97);
+  image(video, 0, 0, width, height);
 
-    if(trackingData){ 
-      for (var i = 0; i < trackingData.length; i++) { 
-       // console.log( trackingData[i] );
-        if(puede==0){
-      
+  if (trackingData) {
+    for (var i = 0; i < trackingData.length; i++) {
+      // console.log( trackingData[i] );
+      if (puede == 0) {
         CamPlatarforma();
-        
+
         desbloquear();
-        }
-        puede++;
-        //rect(trackingData[i].x,trackingData[i].y,trackingData[i].width,trackingData[i].height)
       }
+      puede++;
+      //rect(trackingData[i].x,trackingData[i].y,trackingData[i].width,trackingData[i].height)
+    }
+  }
+
+  for (let i = plataformas.length - 1; i >= 0; i--) {
+    plataformas[i].show();
+
+    if (plataformas[i].hits(ball)) {
     }
 
-    for(let i= plataformas.length-1; i >= 0; i--){
-        plataformas[i].show();
-      
-
-        if(plataformas[i].hits(ball)){
-            
-        }
-
-
-        if (plataformas[i].offscreen()){
-            plataformas.splice(i,1);
-        }
-        
+    if (plataformas[i].offscreen()) {
+      plataformas.splice(i, 1);
     }
+  }
 
-    ball.update();
-    ball.show();
-
-    
+  ball.update();
+  ball.show();
 }
 
-function keyPresionado(){
+function keyPresionado() {
+  if (keyIsDown(LEFT_ARROW)) {
+    ball.MoveLeft();
+  }
+  if (keyIsDown(RIGHT_ARROW)) {
+    ball.MoveRight();
+  }
+  if (key == "j" || key == "J") {
+    ball.Jump();
+  }
+}
 
-    if(keyIsDown(LEFT_ARROW) ){  
-       ball.MoveLeft();
-    }
-    if(keyIsDown(RIGHT_ARROW)){ 
-       ball.MoveRight();
-    }
-    if(key == 'j' || key == 'J'){ 
-        ball.Jump();
-     }
-    
- }   
-
-
-
- function CamPlatarforma(){
+function CamPlatarforma() {
   plataformas.push(new Plataforma());
   separacion_y -= 90;
-  plataformas[plataformas.length-1].crear(separacion_y);
+  plataformas[plataformas.length - 1].crear(separacion_y);
+}
 
- }
-
-function desbloquear(){
-setTimeout(function(){
- puede=0;
-}, 3000);
-
+function desbloquear() {
+  setTimeout(function () {
+    puede = 0;
+  }, 3000);
 }
